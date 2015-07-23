490f5432e5a81a1425bc72a6f625480e8491321c
sky <sky@sky-PC>
2015-07-22 21:16:14 -0400

    prototype BaseController

--- SkyEngine/src/me/skykistler/engine/controller/BaseController.java
+++ SkyEngine/src/me/skykistler/engine/controller/BaseController.java
@@ -5,12 +5,31 @@ import me.skykistler.engine.view.View;
 
 public class BaseController implements Controller {
 
-	public void init(Model model, View view) {
+	private Model model;
+	private View view;
+	private Input input;
 
+	public void init(Model model, View view) {
+		this.model = model;
+		this.view = view;
+
+		input = new BaseInput();
+
+		// TODO init and pass viewport
+		view.init(model, null);
+
+		model.init();
+
+		input.init(view);
 	}
 
 	public void update() {
+		input.update();
 
+		model.update();
+
+		// TODO time rendering
+		view.render();
 	}
 
 }
